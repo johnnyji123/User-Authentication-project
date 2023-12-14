87963e5259f5f97f0c56a8f12668f0c4500fbeb1
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-14 13:48:59 +0000

    configured flask mail

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ cursor = db.cursor()
 
 app = Flask(__name__)
 app.secret_key = "123123123"
+
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'sendcrypto123456@gmail.com'
+app.config['MAIL_PASSWORD'] = 'dgvbwzratthkvyvn'
+app.config['MAIL_USE_TLS'] = True
 mail = Mail(app)
 
 @app.route("/" , methods = ["GET", "POST"])
@@ -51,23 +58,34 @@ def register_user():
     email = request.form.get("Email")
     password = request.form.get("Password")
     
-    msg = Message("Please verify your email", sender = "jj_sasuke@hotmail.co.uk", recipient = email)
-    msg.body = f"http://127.0.0.1:5000/{random_token}"
+    msg = Message("Please verify your email", sender = "jj_sasuke@hotmail.co.uk", recipients = [email])
+    msg.body = f"http://127.0.0.1:8000/verify_email?token={random_token}"
     mail.send(msg)
     
     cursor.execute("INSERT INTO user_info (Name, Email, Password, Token) VALUES (%s, %s, %s, %s)",
                   (name, email, password, random_token) )
         
+    db.commit()
     
     
     return render_template("register_user.html")
 
-@app.route("/verify_email/<token>", methods = ["GET", "POST"])
-def email_verification(token):
+@app.route("/verify_email", methods = ["GET", "POST"])
+def email_verification():
+    cursor.execute("SELECT Token FROM user_info")
+    token_data = cursor.fetchall()
+    extract_token_url = request.args.get('token')
+    
+    for token in token_data:
+        if token[0] == extract_token_url:
+            cursor.execute("UPDATE user_info SET Token = 'True' WHERE Token = %s", (extract_token_url))
+            db.commit()
+            return redirect("/")
+                
         
-    return render_template("verify_email.html")
-        
+       
+    
 
 if __name__ == "__main__":
-    app.run(debug= True, use_reloader = False)
+    app.run(debug= True, port = 8000 , use_reloader = False)
 
